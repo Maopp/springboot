0211e2d9bb83fc15896715f11120a00e5a50a81e
Maopp <1121266440@qq.com>
2018-08-21 22:50:22 +0800

    整合mybatis

---
+++ src/main/java/com/catpp/springboot/mapper/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.catpp.springboot.mapper;
+
+import com.catpp.springboot.pojo.SysUser;
+import com.catpp.springboot.utils.MyMapper;
+
+public interface SysUserMapper extends MyMapper<SysUser> {
+}
